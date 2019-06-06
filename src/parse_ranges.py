9351162d5700cffad7b2f6b1d30706e6d800e2f6
Shuchen Wu <shuchen.wu2@gmail.com>
2019-06-06 12:46:19 +0800

    practice of parse_ranges

---
+++ src/parse_ranges.py
@@ -0,0 +1,15 @@
+import re
+
+
+def parse_ranges(string):
+    p = re.compile(r'(?P<start>\d+)-?(?P<end>\d+)?')
+    match = re.finditer(p, string)
+    ls = list()
+
+    for m in match:
+        begin = int(m.group('start'))
+        end = int(m.group('end'))+1 if m.group('end') is not None else begin+1
+        ls.append(i for i in range(begin, end))  # somehow a list of generators
+        for item in ls:
+            for value in item:
+                yield value
